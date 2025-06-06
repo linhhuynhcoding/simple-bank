af370a97cb2f6fc4bb655ac5de6d58254a366c66
linhhuynhcoding <nhatlinh.6449@gmail.com>
2025-06-06 11:03:58 +0700

    feat: generate sqlc transferdb & entrydb

--- db/accountdb/account_test.go
+++ db/accountdb/account_test.go
@@ -2,12 +2,15 @@ package accountdb
 
 import (
 	"context"
+	"database/sql"
 	"testing"
+	"time"
+
 	"github.com/linhhuynhcoding/learn-go/util"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+func createRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
 		Owner:    util.RandomOwner(),
 		Balance:  util.RandomMoney(),
@@ -20,8 +23,59 @@ func TestCreateAccount(t *testing.T) {
 
 	require.Equal(t, arg.Owner, account.Owner)
 	require.Equal(t, arg.Currency, account.Currency)
-	require.Equal(t, arg.Currency, account.Currency)
+	require.Equal(t, arg.Balance, account.Balance)
 
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account2.ID, account2.ID)
+	require.Equal(t, account2.Balance, account2.Balance)
+	require.Equal(t, account2.Owner, account2.Owner)
+	require.Equal(t, account2.Currency, account2.Currency)
+	require.WithinDuration(t, account2.CreatedAt, account2.CreatedAt, time.Second)
+}
+
+func TestUpdateAccount(t *testing.T) {
+	account := createRandomAccount(t)
+
+	arg := UpdateAccountParams{
+		ID:      account.ID,
+		Balance: util.RandomMoney(),
+	}
+
+	newAccount, err := testQueries.UpdateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, newAccount)
+
+	require.Equal(t, account.ID, newAccount.ID)
+	require.Equal(t, arg.Balance, newAccount.Balance)
+	require.Equal(t, account.Owner, newAccount.Owner)
+	require.Equal(t, account.Currency, newAccount.Currency)
+	require.WithinDuration(t, account.CreatedAt, newAccount.CreatedAt, time.Second)
+}
+
+func TestDeleteAccount(t *testing.T) {
+	account := createRandomAccount(t)
+
+	err := testQueries.DeleteAccount(context.Background(), account.ID)
+	require.NoError(t, err)
+	
+	account2, err2 := testQueries.GetAccount(context.Background(), account.ID)
+	require.Error(t, err2)
+	require.EqualError(t, err2, sql.ErrNoRows.Error())
+	require.Empty(t, account2)
 }
---
+++ db/entrydb/query/entry.sql
@@ -0,0 +1,10 @@
+-- name: CreateEntry :one
+INSERT INTO
+    entries (account_id, amount)
+VALUES ($1, $2) RETURNING *;
+
+-- name: GetEntry :one
+SELECT * FROM entries WHERE account_id = $1 LIMIT 1;
+
+-- name: ListEntry :many
+SELECT * FROM entries ORDER BY created_at DESC LIMIT $1 OFFSET $2;
